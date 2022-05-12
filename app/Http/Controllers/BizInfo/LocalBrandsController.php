f0fa1e057b19a99ad20d70c8bad547b11c284481
hekim <hekimyazrow@gmail.com>
2022-05-12 16:13:30 +0500

    recovery sait v1.4.1 (import in excel)

--- app/Http/Controllers/BizInfo/LocalBrandsController.php
+++ app/Http/Controllers/BizInfo/LocalBrandsController.php
@@ -4,14 +4,20 @@ namespace App\Http\Controllers\BizInfo;
 
 
 use App\Http\Controllers\Controller;
+use App\Imports\BrandsImport;
 use App\Models\Brands;
+use Maatwebsite\Excel\Facades\Excel;
 
 class LocalBrandsController extends Controller
 {
     public function __invoke()
     {
+//        Excel::import(new BrandsImport(), public_path('telekeciler.xlsx'));
+
         $brands = Brands::orderByDesc('id')->paginate(12);
+
         $title = 'Local brands';
+
         return view('org.biz_info.local_brands', compact('title', 'brands'));
     }
 }
