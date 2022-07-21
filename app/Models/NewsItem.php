ed594268ac16d590f44b51c7a01dc8c036ff1a5d
HekimWebDev <hekimyazrow@gmail.com>
2022-07-21 16:31:10 +0500

    add rss v1

--- app/Models/NewsItem.php
+++ app/Models/NewsItem.php
@@ -12,14 +12,14 @@ class NewsItem extends News implements Feedable
     public function toFeedItem(): FeedItem
     {
 //        dd([$this->publish_at, now()]);
-        return FeedItem::create()
-            ->id($this->id)
-            ->title($this->title)
-            ->summary($this->desc)
-            ->updated(Carbon::parse($this->publish_at ))
-            ->link(route('news_single', $this->slug))
-            ->authorName('')
-            ->authorEmail('');
+        return FeedItem::create([
+            'id'         => $this->id,
+            'title'      => $this->title,
+            'summary'    => $this->desc,
+            'updated'    => Carbon::parse($this->publish_at),
+            'link'       => route('news_single', $this->slug),
+            'authorName' => 'cci.gov.tm',
+        ]);
     }
 
     public static function getFeedItems()
