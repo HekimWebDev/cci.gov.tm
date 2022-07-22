31221b56396e58ea0e6ad384549ef7085348c877
HekimWebDev <hekimyazrow@gmail.com>
2022-07-22 23:17:25 +0500

    change fortify to default auth v1.1

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -22,7 +22,7 @@ class AdminMiddleware
             return $next($request);
         }
 
-        return redirect()->route('login');
+        return redirect()->route('auth.login.page');
 
         // abort(404);
     }
